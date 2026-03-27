43d6a848baefa9b887d71f5f7d8a3bee4bf15fb4
Carlos Terrero <carlos.terrero@mylighthouse.com>
2026-03-27 14:35:47 +0100

    1st try for inMemoryProductRepositoryTest

---
+++ basic_crud/tests/Data/Product/Infrastructure/Repository/InMemoryProductRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Data\Product\Infrastructure\Repository;
+
+use App\Product\Entity\Product;
+use App\Product\Repository\ProductRepositoryInterface;
+
+final class InMemoryProductRepository implements ProductRepositoryInterface
+{
+    private array $products = [];
+
+    public function findAll(): array
+    {
+        return array_values($this->products);
+    }
+
+    public function find(string $id): ?Product
+    {
+        return $this->products[$id] ?? null;
+    }
+
+    public function save(Product $entity): void
+    {
+        $this->products[$entity->id()] = $entity;
+    }
+
+    public function remove(Product $entity): void
+    {
+        unset($this->products[$entity->id()]);
+    }
+
+    public function findByName(string $name): array
+    {
+        return array_values(array_filter(
+            $this->products,
+            fn(Product $product) => $product->name() === $name
+        ));
+    }
+}
